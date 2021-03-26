afe020f72149d076640f11ff757c829f78042cc0
Domagoj <domagoj.borovcak@gmail.com>
2021-03-26 17:03:19 +0100

    Upgrade to last one

--- src/main/java/hr/java/covidportal/model/Osoba.java
+++ src/main/java/hr/java/covidportal/model/Osoba.java
@@ -2,6 +2,7 @@ package main.java.hr.java.covidportal.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 import java.util.Arrays;
 import java.util.Optional;
@@ -16,13 +17,13 @@ public class Osoba {
     Bolest zarazenBolescu;
     Osoba[] kontaktiraneOsobe;
 
-//    public Osoba(String ime, String prezime, Integer starost, Zupanija zupanija, Bolest zarazenBolescu) {
-//        this.ime = ime;
-//        this.prezime = prezime;
-//        this.starost = starost;
-//        this.zupanija = zupanija;
-//        this.zarazenBolescu = zarazenBolescu;
-//    }
+    public Osoba(String ime, String prezime, Integer starost, Zupanija zupanija, Bolest zarazenBolescu) {
+        this.ime = ime;
+        this.prezime = prezime;
+        this.starost = starost;
+        this.zupanija = zupanija;
+        this.zarazenBolescu = zarazenBolescu;
+    }
 //    public Osoba(String ime, String prezime, Integer starost, Zupanija zupanija, Bolest zarazenBolescu, Osoba[] kontaktiraneOsobe) {
 //        this.ime = ime;
 //        this.prezime = prezime;
